de6caff1313865837151fbf5e549074df37180db
Arman Hossain <armaanepiicfb17@gmail.com>
2024-12-05 15:10:08 +0600

    navbar and Dark theme competed

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1400px",
+    },
+    extend: {
+      colors: {
+        sectionColor: "hsl(209, 100%, 97%)",
+        darkBodyColor: "hsl(216, 100%, 4%)",
+        darkSectionColor: "hsl(211, 100%, 12%)",
+        primaryColor: "hsl(209, 87%, 21%)",
+        primaryColorLight: "hsl(209, 74%, 45%)",
+        whiteColor: "#fff",
+        textColor: "#DDD",
+        secondaryColor: "red",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "10px",
+        md: "30px",
+      },
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
+
